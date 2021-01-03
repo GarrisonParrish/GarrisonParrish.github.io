86f76da5a88dfd011eeec6672325638170ea7152
Garrison Parrish <tpgarris@live.unc.edu>
2021-01-02 20:31:38 -0500

    fixed color switcher

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 'use strict'  // Activate strict mode, avoids making stupid assumptions like Garrison in a math class
 
-const switcher = document.querySelector('.theme_switcher_btn');   // gets button reference
+const switcher = document.querySelector('.theme-switcher-btn');   // gets button reference
 
-switcher.addEventListener('click', function() {  // listener for click event--function  passed in is actual event handler
+switcher.addEventListener('click', function() {  // listener for click event--function passed in is actual event handler
     document.body.classList.toggle('mellow-theme')  // toggle method switches element to mellow-theme class
 
     var className = document.body.className;
-    if(className == "light-theme") {
-        this.textContent = "Mellow";
+    if(className == "mellow-theme") {
+        this.textContent = "Light";
     }
     else {
-        this.textContent = "Light";
+        this.textContent = "Mellow";
     }
 
     console.log('current class name: ' + className);  // doesn't appear on the webpage, but useful for debugging
